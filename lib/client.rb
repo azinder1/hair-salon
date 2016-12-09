f0b373dc9575e529d366c55d35f64cf985feb4e1
Adam Zinder <adam.zinder@gmail.com>
2016-12-09 10:08:05 -0800

    client class updated to include stylist_id attribute with all specs passing

--- lib/client.rb
+++ lib/client.rb
@@ -1,10 +1,11 @@
 class Client
-  attr_reader(:name, :contact_number, :gender, :id)
+  attr_reader(:name, :contact_number, :gender, :stylist_id, :id)
 
   def initialize(attributes)
     @name = attributes[:name]
     @contact_number = attributes[:contact_number]
     @gender = attributes[:gender]
+    @stylist_id = attributes[:stylist_id]
     @id = attributes.fetch(:id, nil)
   end
 
@@ -12,30 +13,33 @@ class Client
     return_clients = DB.exec("Select * FROM clients;")
     clients = []
     return_clients.each do |client|
-      current_client = Client.new(:name => client['name'], :contact_number => client['contact_number'], :gender => client['gender'], :id => client['id'].to_i)
+      current_client = Client.new(:name => client['name'], :contact_number => client['contact_number'], :gender => client['gender'], :stylist_id => client['stylist_id'], :id => client['id'].to_i)
       clients.push(current_client)
     end
     clients
   end
 
   def save
-    result = DB.exec("INSERT INTO clients(name, contact_number, gender) VALUES ('#{@name}', '#{@contact_number}', '#{@gender}') RETURNING id;")
+    result = DB.exec("INSERT INTO clients(name, contact_number, gender, stylist_id) VALUES ('#{@name}', '#{@contact_number}', '#{@gender}', #{@stylist_id}) RETURNING id;")
     @id = result.first().fetch('id').to_i
   end
 
   def self.find(id)
     found_client = DB.exec("SELECT * FROM clients WHERE id = '#{id}'").first
-    Client.new(:name => found_client['name'], :contact_number => found_client['contact_number'], :gender =>found_client['gender'], :id => found_client['id'].to_i)
+    Client.new(:name => found_client['name'], :contact_number => found_client['contact_number'], :gender =>found_client['gender'], :stylist_id => found_client['stylist_id'].to_i, :id => found_client['id'].to_i)
   end
+
   def ==(another_client)
-   (self.name && self.contact_number && self.gender) == (another_client.name && another_client.contact_number && another_client.gender)
+   (self.name && self.contact_number && self.gender && self.stylist_id) == (another_client.name && another_client.contact_number && another_client.gender && another_client.stylist_id)
  end
+
   def update(attributes)
     @id = self.id
     @name = attributes.fetch(:name, @name)
     @contact_number = attributes.fetch(:contact_number, @contact_number)
     @gender = attributes.fetch(:gender, @gender)
-    update_result = DB.exec("UPDATE clients SET name = '#{@name}', contact_number = '#{@contact_number}', gender = '#{@gender}' WHERE id = #{@id};")
+    @stylist_id = attributes.fetch(:stylist_id, @stylist_id)
+    update_result = DB.exec("UPDATE clients SET name = '#{@name}', contact_number = '#{@contact_number}', gender = '#{@gender}', stylist_id = #{stylist_id} WHERE id = #{@id};")
   end
 
   def delete
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -8,26 +8,27 @@ describe(Client) do
   end
   describe('.find') do
     it('returns a client by their ID number') do
-      client1 = Client.new(:name => "Clyde", :contact_number => "555-555-5555", :gender => "Male")
+      client1 = Client.new(:name => "Clyde", :contact_number => "555-555-5555", :gender => "Male", :stylist_id => 3)
       client1.save
-      client2 = Client.new(:name => "Melissa", :contact_number => "444-444-4444", :gender => "Female")
+      client2 = Client.new(:name => "Melissa", :contact_number => "444-444-4444", :gender => "Female", :stylist_id => 1)
       client2.save
       expect(Client.find(client1.id)).to(eq(client1))
     end
   end
   describe("#delete") do
     it('lets you delete a client from the database') do
-      client1 = Client.new(:name => "Clyde", :contact_number => "555-555-5555", :gender => "Male")
+      client1 = Client.new(:name => "Clyde", :contact_number => "555-555-5555", :gender => "Male", :stylist_id => 3)
       client1.save
-      client2 = Client.new(:name => "Melissa", :contact_number => "444-444-4444", :gender => "Female")
+      client2 = Client.new(:name => "Melissa", :contact_number => "444-444-4444", :gender => "Female", :stylist_id => 1)
       client2.save
       client1.delete
-      expect(Client.all).to(eq([client2]))
+      client2.delete
+      expect(Client.all).to(eq([]))
     end
   end
   describe("#update") do
     it('lets you update a client specs in a database') do
-      client1 = Client.new(:name => "Clyde", :contact_number => "555-555-5555", :gender => "Male")
+      client1 = Client.new(:name => "Clyde", :contact_number => "555-555-5555", :gender => "Male", :stylist_id => 3)
       client1.save
       client1.update(:name => "Clevis")
       expect(client1.name).to(eq("Clevis"))
